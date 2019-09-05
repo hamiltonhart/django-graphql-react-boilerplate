e80f6700204da59aaf5a26b063c934416588acd5
George Hart <hamiltonhart@protonmail.com>
2019-09-05 05:16:09 -0700

    GraphQL setup in React. Still Needs jwt and some other things. Django still needs Custom User and environment variables.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const link = new HttpLink({
+  uri: "http://localhost:8000/graphql/"
+  // uri: "http://example.herokuapp.com/graphql/"
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link,
+  cache,
+  dataIdFromObject: o => o.id
+});
+
+const ApolloApp = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+)
+
+ReactDOM.render(<ApolloApp />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
