e4a9c0953fa2e04ded58883b0bd2ea3a5cbc13f8
George Hart <hamiltonhart@protonmail.com>
2019-09-09 13:15:32 -0700

    Adjusting main index.js file to properly incorporate JWT authorization. Testing now.

--- src/index.js
+++ src/index.js
@@ -1,54 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import Auth from './components/Auth';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Auth from "./components/Auth";
 
-import { ApolloProvider, Query } from "react-apollo";
+// import { ApolloProvider, Query } from "react-apollo";
+import { ApolloProvider, useQuery } from "@apollo/react-hooks";
 import ApolloClient, { gql } from "apollo-boost";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from "./serviceWorker";
 // uri: "http://example.herokuapp.com/graphql/"
 
 // !Apollo Client is available to the entire app
 // ! fetchOptions: {credentials: 'include' - means to include operation.headers (just below in the request)}
+const cache = new InMemoryCache();
 const client = new ApolloClient({
   uri: "http://localhost:8000/graphql/",
   fetchOptions: {
     credentials: "include"
   },
   request: operation => {
-    const token = localStorage.getItem('authToken') || "";
+    const token = localStorage.getItem("authToken") || "";
     operation.setContext({
       headers: {
         Authorization: `JWT ${token}`
       }
-    })
-  },
-  clientState: {
-    defaults: {
-      // !The double bangs converts any value to a boolean
-      isLoggedIn: !!localStorage.getItem("authToken")
-    }
+    });
   }
 });
 
-// Does a query on the client (not the server) and the returned value (boolean) determines what the user sees, in this case home page or authorization page
-const IS_LOGGED_IN_QUERY = gql`
-  query {
-    isLoggedIn @client
+cache.writeDate({
+  data: {
+    isLoggedIn: !!localStorage.getItem("authToken")
   }
-`;
-
+});
 
 // ! Change the turnary below to include an authorization page, or whatever the user should see if they are not logged in.
-const ApolloApp = () => (
-  <ApolloProvider client={client}>
-    <Query query={IS_LOGGED_IN_QUERY}>
-      {({ data }) => (data.isLoggedIn ? <App /> : <Auth />)}
-    </Query>
-  </ApolloProvider>
-)
+const ApolloApp = () => {
+  const { loading, error, data } = useQuery(gql`
+    {
+      isLoggedIn @client
+    }
+  `);
+
+  return (
+    <ApolloProvider client={client}>
+      {console.log(data)}
+      {data => (data.isLoggedIn ? <App /> : <Auth />)}
+    </ApolloProvider>
+  );
+};
 
 ReactDOM.render(<ApolloApp />, document.getElementById("root"));
 
@@ -56,3 +58,31 @@ ReactDOM.render(<ApolloApp />, document.getElementById("root"));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// const client = new ApolloClient({
+//   uri: "http://localhost:8000/graphql/",
+//   fetchOptions: {
+//     credentials: "include"
+//   },
+//   request: operation => {
+//     const token = localStorage.getItem('authToken') || "";
+//     operation.setContext({
+//       headers: {
+//         Authorization: `JWT ${token}`
+//       }
+//     })
+//   },
+//   clientState: {
+//     defaults: {
+//       // !The double bangs converts any value to a boolean
+//       isLoggedIn: !!localStorage.getItem("authToken")
+//     }
+//   }
+// });
+
+// Does a query on the client (not the server) and the returned value (boolean) determines what the user sees, in this case home page or authorization page
+// const IS_LOGGED_IN_QUERY = gql`
+//   query {
+//     isLoggedIn @client
+//   }
+// `;
